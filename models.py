87b795596a0a9c7ec4f66a6d302b264432ccd2cb
boo <booblegum42@gmail.com>
2025-09-19 17:02:59 +0700

    model Category

--- models.py
+++ models.py
@@ -1,7 +1,16 @@
-from sqlalchemy import Column, Integer, String, DateTime, Text, func
-from sqlalchemy.orm import Mapped, mapped_column
+from typing import Optional
+from sqlalchemy import Column, Integer, String, DateTime, Text, func, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from database import Base
 
+
+class Category(Base):
+    __tablename__ = "categories"
+
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    name: Mapped[str] = mapped_column(String, unique=True, index=True)
+    description: Mapped[str] = mapped_column(Text, default='')
+    lost_items: Mapped[list["LostItem"]] = relationship(back_populates="category")
 
 class LostItem(Base):
     __tablename__ = "lost_items"
@@ -11,6 +20,8 @@ class LostItem(Base):
     description: Mapped[str] = mapped_column(Text, default='')
     lost_date: Mapped[DateTime] = mapped_column(DateTime, default=func.now())
     location: Mapped[str] = mapped_column(String)
+    category_id: Mapped[Optional[int]] = mapped_column(ForeignKey("categories.id"))
+    category: Mapped[Optional["Category"]] = relationship(back_populates="lost_items")
 
 
 class FoundItem(Base):
--- schemas/lost_items.py
+++ schemas/lost_items.py
@@ -4,8 +4,9 @@ from datetime import datetime
 
 # LostItems
 class LostItemBase(BaseModel):
+    category_id: int | None = None
     name: str
-    description: str = None
+    description: str | None = None
     lost_date: datetime = None
     location: str
 
@@ -23,3 +24,25 @@ class LostItemUpdate(BaseModel):
 
 class LostItem(LostItemBase):
     id: int
+
+# + легкий
+# + быстрый
+# - доп.запрос
+# GET: /items/{id}
+item = {
+    "name": ...,
+    "location": ...,
+    "category_id": 2 # GET: /categories/2
+}
+
+# GET: /items/{id}?include=category
+# - большая
+# - дольше
+item = {
+    "name": ...,
+    "location": ...,
+    "category": {
+        "id": 2,
+        "title": "Электроника",
+    }
+}
